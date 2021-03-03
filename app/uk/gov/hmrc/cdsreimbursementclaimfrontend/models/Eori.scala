01a5adbdbf0c059aebb79ed9eaebba5f3a103265
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2021-03-03 13:20:12 +0000

    CDSR-375: Change regex for eori number and modify error messasges

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/models/Eori.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/models/Eori.scala
@@ -24,7 +24,7 @@ final case class Eori(value: String) extends AnyVal
 object Eori {
 
   def isValid(maybeEori: String): Boolean = {
-    val regex = """\w{14, 17}"""
+    val regex = """\w{14,17}"""
     maybeEori.matches(regex)
   }
 
