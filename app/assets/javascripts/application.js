de0f3a2423d15ed24bca80630de1a82aa1694854
mikail-khan <mikail.khan@digital.hmrc.gov.uk>
2021-03-04 17:19:15 +0000

    CDSR-125: add service JS

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,13 @@
+window.GOVUKFrontend.initAll();
+window.HMRCFrontend.initAll();
+
+// =====================================================
+// Back link mimics browser back functionality
+// =====================================================
+// store referrer value to cater for IE - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/10474810/  */
+const docReferrer = document.referrer
+
+// prevent resubmit warning
+if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
+  window.history.replaceState(null, null, window.location.href);
+}
