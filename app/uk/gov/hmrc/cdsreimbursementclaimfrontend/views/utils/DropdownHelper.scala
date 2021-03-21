2b464c361e586daca409edb4409008874e1194af
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2021-03-21 16:08:24 +0000

    CDSR-524: create DropDownHelper for dropdown details component

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/views/utils/DropdownHelper.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/views/utils/DropdownHelper.scala
@@ -24,23 +24,46 @@ import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.components.paragraph
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.components.bullets
 
 class DropdownHelper @Inject() (paragraph: paragraph_block, bulletList: bullets) extends htmlUtil {
-  val landingKey: String = "landing"
+
+  val landingKey: String          = "landing.details"
+  val supportingEvidenceUploadKey = "supporting-evidence.upload.details"
+  val reasonAndBasisKey           = "select-reason-and-basis-for-claim.basis"
+  val basisKey                    = "select-reason-and-basis-for-claim.reason"
+  val whoIsMakingTheClaimKey      = "select-who-is-making-the-claim"
 
   def landingDropDownContent()(implicit messages: Messages): Html =
     html(
       paragraph(Html(messages(s"$landingKey.details.p1")), Some("govuk-body")),
       bulletList(
         Seq(
-          Html(messages(s"$landingKey.details.l1")),
-          Html(messages(s"$landingKey.details.l2")),
-          Html(messages(s"$landingKey.details.l3")),
-          Html(messages(s"$landingKey.details.l4")),
-          Html(messages(s"$landingKey.details.l5")),
-          Html(messages(s"$landingKey.details.l6")),
-          Html(messages(s"$landingKey.details.l7")),
-          Html(messages(s"$landingKey.details.l8")),
-          Html(messages(s"$landingKey.details.l9")),
-          Html(messages(s"$landingKey.details.l10"))
+          Html(messages(s"$landingKey.l1")),
+          Html(messages(s"$landingKey.l2")),
+          Html(messages(s"$landingKey.l3")),
+          Html(messages(s"$landingKey.l4")),
+          Html(messages(s"$landingKey.l5")),
+          Html(messages(s"$landingKey.l6")),
+          Html(messages(s"$landingKey.l7")),
+          Html(messages(s"$landingKey.l8")),
+          Html(messages(s"$landingKey.l9")),
+          Html(messages(s"$landingKey.l10"))
+        )
+      )
+    )
+
+  def uploadDropDownContent()(implicit messages: Messages): Html =
+    html(
+      bulletList(
+        Seq(
+          Html(messages(s"$supportingEvidenceUploadKey.l0")),
+          Html(messages(s"$supportingEvidenceUploadKey.l1")),
+          Html(messages(s"$supportingEvidenceUploadKey.l2")),
+          Html(messages(s"$supportingEvidenceUploadKey.l3")),
+          Html(messages(s"$supportingEvidenceUploadKey.l4")),
+          Html(messages(s"$supportingEvidenceUploadKey.l5")),
+          Html(messages(s"$supportingEvidenceUploadKey.l6")),
+          Html(messages(s"$supportingEvidenceUploadKey.l7")),
+          Html(messages(s"$supportingEvidenceUploadKey.l8")),
+          Html(messages(s"$supportingEvidenceUploadKey.l9"))
         )
       )
     )
