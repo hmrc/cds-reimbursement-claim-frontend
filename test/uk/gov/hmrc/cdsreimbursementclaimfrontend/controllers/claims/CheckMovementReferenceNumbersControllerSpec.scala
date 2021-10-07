914561616cf857f5ebb09f33bd362e55081d8763
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2021-10-07 15:30:15 +0100

    CDSR-939: create CheckMovementReferenceNumbersControllerSpec

---
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/claims/CheckMovementReferenceNumbersControllerSpec.scala
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.claims
+
+import cats.data.EitherT
+import cats.implicits._
+import org.scalatest.OptionValues
+import org.jsoup.nodes.Document
+import play.api.i18n.{Lang, Messages, MessagesApi, MessagesImpl}
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.cache.SessionCache
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.{AuthSupport, ControllerSpec, SessionSupport, routes => baseRoutes}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.DraftClaim.DraftC285Claim
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.JourneyStatus.FillingOutClaim
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.declaration.DisplayDeclaration
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.generators.Generators.sample
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.generators.IdGen._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.generators.SignedInUserDetailsGen._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.ids.{GGCredId, MRN}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.services.{ClaimService, FeatureSwitchService}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class CheckMovementReferenceNumbersControllerSpec
+    extends ControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with OptionValues {
+
+  val mockClaimsService: ClaimService = mock[ClaimService]
+
+  override val overrideBindings: List[GuiceableModule] =
+    List[GuiceableModule](
+      bind[AuthConnector].toInstance(mockAuthConnector),
+      bind[SessionCache].toInstance(mockSessionCache),
+      bind[ClaimService].toInstance(mockClaimsService)
+    )
+
+  def mockGetDisplayDeclaration(response: Either[Error, Option[DisplayDeclaration]]) =
+    (mockClaimsService
+      .getDisplayDeclaration(_: MRN)(_: HeaderCarrier))
+      .expects(*, *)
+      .returning(EitherT.fromEither[Future](response))
+
+  val controller: CheckMovementReferenceNumbersController = instanceOf[CheckMovementReferenceNumbersController]
+  val featureSwitch: FeatureSwitchService                 = instanceOf[FeatureSwitchService]
+
+  implicit val messagesApi: MessagesApi = controller.messagesApi
+  implicit val messages: Messages       = MessagesImpl(Lang("en"), messagesApi)
+
+  private def sessionWithClaimState(
+    maybeMovementReferenceNumberAnswer: Option[MovementReferenceNumber],
+    numberOfClaims: Option[SelectNumberOfClaimsAnswer]
+  ): (SessionData, FillingOutClaim, DraftC285Claim) = {
+    val draftC285Claim      =
+      DraftC285Claim.newDraftC285Claim.copy(
+        movementReferenceNumber = maybeMovementReferenceNumberAnswer,
+        selectNumberOfClaimsAnswer = numberOfClaims
+      )
+    val ggCredId            = sample[GGCredId]
+    val signedInUserDetails = sample[SignedInUserDetails]
+    val journey             = FillingOutClaim(ggCredId, signedInUserDetails, draftC285Claim)
+    (
+      SessionData.empty.copy(
+        journeyStatus = Some(journey)
+      ),
+      journey,
+      draftC285Claim
+    )
+  }
+
+  def getErrorSummary(document: Document): String =
+    document.select(".govuk-error-summary__list > li > a").text()
+
+  "CheckMovementReferenceNumbersController" must {
+    def performAction(): Future[Result] = controller.showMrns()(FakeRequest())
+
+    "redirect to the start of the journey" when {
+
+      "there is no journey status in the session" in {
+        val (session, _, _) = sessionWithClaimState(None, None)
+
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session.copy(journeyStatus = None))
+        }
+
+        checkIsRedirect(
+          performAction(),
+          baseRoutes.StartController.start()
+        )
+      }
+    }
+
+  }
+}
