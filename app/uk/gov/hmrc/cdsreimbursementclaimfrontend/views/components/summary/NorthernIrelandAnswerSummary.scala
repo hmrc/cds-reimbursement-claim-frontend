99bcd9036ce02f2bc66e666ac8825b60b0d89b69
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2021-11-15 10:56:06 +0000

    CDSR-1029 Working on Northern Ireland CYA summary

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/views/components/summary/NorthernIrelandAnswerSummary.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/views/components/summary/NorthernIrelandAnswerSummary.scala
@@ -1,35 +1,27 @@
 package uk.gov.hmrc.cdsreimbursementclaimfrontend.views.components.summary
 
 import play.api.i18n.Messages
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.claims.routes
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.ReimbursementRoutes.ReimbursementRoutes
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.answers.YesNo
-import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryList
+import uk.gov.hmrc.govukfrontend.views.Aliases.{ActionItem, Key, Text}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{Actions, SummaryList, SummaryListRow, Value}
 
 class NorthernIrelandAnswerSummary extends AnswerSummary[YesNo] {
 
   def render(key: String, answer: YesNo)(implicit router: ReimbursementRoutes, messages: Messages): SummaryList =
     SummaryList(
-
-    )
-
-
-  /*
-
-    def makeNorthernIrelandClaimSummary(
-    completeClaim: CompleteClaim
-  )(implicit messages: Messages, journey: JourneyBindable): List[SummaryListRow] =
-    completeClaim.northernIrelandAnswer.fold(List.empty[SummaryListRow])(claimNorthernIrelandAnswer =>
-      List(
+      Seq(
         SummaryListRow(
-          key = Key(Text(messages(s"$key.northern-ireland-claim.label"))),
-          value = Value(Text(claimNorthernIrelandAnswer.toString)),
+          key = Key(Text(messages(s"$key.label"))),
+          value = Value(Text(answer.toString)),
           actions = Some(
             Actions(
               items = Seq(
                 ActionItem(
-                  href = s"${routes.ClaimNorthernIrelandController.changeNorthernIrelandClaim(journey).url}",
+                  href = s"${routes.ClaimNorthernIrelandController.changeNorthernIrelandClaim(router.journeyBindable).url}",
                   content = Text(messages("cya.change")),
-                  visuallyHiddenText = Some(messages(s"$key.northern-ireland-claim.label"))
+                  visuallyHiddenText = Some(messages(s"$key.label"))
                 )
               )
             )
@@ -37,5 +29,4 @@ class NorthernIrelandAnswerSummary extends AnswerSummary[YesNo] {
         )
       )
     )
-   */
 }
