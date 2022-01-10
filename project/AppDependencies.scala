645e9fd97c3a855face9e8044aa57fd5ade009cc
Tony Merryfield (Razer Blade 15) <tony.merryfield@oddbunch.co.uk>
2022-01-10 10:05:58 +0000

    CDSR-1425 - Library update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.3.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "1.27.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "1.31.0-play-28",
     "uk.gov.hmrc"       %% "mongo-caching"              % "7.0.0-play-28",
     "uk.gov.hmrc"       %% "play-language"              % "4.13.0-play-28",
     "org.typelevel"     %% "cats-core"                  % "2.3.1",
