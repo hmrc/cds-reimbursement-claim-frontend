4fb975a556e12986f2b06e7c554a4590be2ed756
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2022-01-13 16:34:34 +0000

    CDSR-1138: Create controller, template, form and tests

---
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodssingle/EnterSpecialCircumstancesControllerSpec.scala
@@ -0,0 +1,179 @@
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.rejectedgoodssingle
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.i18n.Lang
+import play.api.i18n.Messages
+import play.api.i18n.MessagesApi
+import play.api.i18n.MessagesImpl
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.cache.SessionCache
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.AuthSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.ControllerSpec
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.SessionSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.Forms.enterSpecialCircumstancesForm
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.RejectedGoodsSingleJourney
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.RejectedGoodsSingleJourneyTestData
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.Feature
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.SessionData
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.services.FeatureSwitchService
+
+import scala.concurrent.Future
+
+class EnterSpecialCircumstancesControllerSpec
+  extends ControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with BeforeAndAfterEach
+    with ScalaCheckPropertyChecks
+    with RejectedGoodsSingleJourneyTestData {
+
+  override val overrideBindings: List[GuiceableModule] =
+    List[GuiceableModule](
+      bind[AuthConnector].toInstance(mockAuthConnector),
+      bind[SessionCache].toInstance(mockSessionCache)
+    )
+
+  val controller: EnterSpecialCircumstancesController = instanceOf[EnterSpecialCircumstancesController]
+
+  implicit val messagesApi: MessagesApi = controller.messagesApi
+  implicit val messages: Messages       = MessagesImpl(Lang("en"), messagesApi)
+
+  private lazy val featureSwitch  = instanceOf[FeatureSwitchService]
+  private val messagesKey: String = "enter-special-circumstances.rejected-goods"
+
+  override def beforeEach(): Unit =
+    featureSwitch.enable(Feature.RejectedGoods)
+
+  val session: SessionData = SessionData.empty.copy(
+    rejectedGoodsSingleJourney = Some(RejectedGoodsSingleJourney.empty(exampleEori))
+  )
+
+  "Enter Special Circumstances Controller" must {
+
+    "not find the page if rejected goods feature is disabled" in {
+      def performAction(): Future[Result] =
+        controller.show()(FakeRequest())
+
+      featureSwitch.disable(Feature.RejectedGoods)
+
+      status(performAction()) shouldBe NOT_FOUND
+    }
+
+    "display the page" when {
+      def performAction(): Future[Result] =
+        controller.show()(FakeRequest())
+
+      "the user has not answered this question before" in {
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$messagesKey.title")
+        )
+      }
+
+    }
+
+    "handle submit requests" must {
+      def performAction(data: (String, String)*): Future[Result] =
+        controller.submit()(FakeRequest().withFormUrlEncodedBody(data: _*))
+
+      "the user has entered some details" in {
+        val journey        = session.rejectedGoodsSingleJourney.getOrElse(fail("No rejected goods journey"))
+        val updatedJourney = journey.submitBasisOfClaimSpecialCircumstancesDetails(exampleSpecialCircumstancesDetails)
+        val updatedSession = session.copy(rejectedGoodsSingleJourney = Some(updatedJourney))
+
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+          mockStoreSession(updatedSession)(Right(()))
+        }
+
+        checkIsRedirect(
+          performAction(controller.formKey -> exampleSpecialCircumstancesDetails),
+          routes.SelectTaxCodesController.show() //FIXME
+        )
+      }
+
+    }
+
+    "show an error summary" when {
+      def performAction(data: (String, String)*): Future[Result] =
+        controller.submit()(FakeRequest().withFormUrlEncodedBody(data: _*))
+
+      "the user submits empty details" in {
+
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(controller.formKey -> ""),
+          messageFromMessageKey(s"$messagesKey.title"),
+          doc =>
+            getErrorSummary(doc) shouldBe messageFromMessageKey(
+              s"$messagesKey.error.required"
+            ),
+          expectedStatus = BAD_REQUEST
+        )
+      }
+
+      "the user submits more than 500 characters" in {
+
+        val answer = List.fill(600)('c').mkString(" ")
+
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(controller.formKey -> answer),
+          messageFromMessageKey(s"$messagesKey.title"),
+          doc =>
+            getErrorSummary(doc) shouldBe messageFromMessageKey(
+              s"$messagesKey.error.maxLength"
+            ),
+          expectedStatus = BAD_REQUEST
+        )
+      }
+
+    }
+  }
+
+  "Form Validation" must {
+    val form     = enterSpecialCircumstancesForm
+    val goodData = Map(
+      messagesKey -> "A box of biscuits"
+    )
+
+    "accept special circumstances details" in {
+      val errors = form.bind(goodData).errors
+      errors shouldBe Nil
+    }
+
+    "special circumstances details" should {
+
+      "Accept longest possible details" in {
+        val errors = form.bind(goodData.updated(messagesKey, List.fill(500)("a").mkString(""))).errors
+        errors shouldBe Nil
+      }
+      "Reject details when it's too long" in {
+        val errors = form.bind(goodData.updated(messagesKey, List.fill(501)("a").mkString(""))).errors
+        errors.headOption.getOrElse(fail()).messages shouldBe List("error.maxLength")
+      }
+    }
+  }
+
+}
+
