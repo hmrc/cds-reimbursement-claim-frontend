3c13fd0fb89fd4bf1df7a48fe6eec84826cf8044
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2022-01-18 12:15:50 +0000

    CDSR-1139: Add test controller

---
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodssingle/EnterInspectionDateControllerSpec.scala
@@ -0,0 +1,100 @@
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.rejectedgoodssingle
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.i18n.Lang
+import play.api.i18n.Messages
+import play.api.i18n.MessagesApi
+import play.api.i18n.MessagesImpl
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.cache.SessionCache
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.AuthSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.ControllerSpec
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.SessionSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.Forms.enterInspectionDateForm
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.RejectedGoodsSingleJourney
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.RejectedGoodsSingleJourneyTestData
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.BasisOfRejectedGoodsClaim
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.Feature
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.SessionData
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.services.FeatureSwitchService
+
+import scala.concurrent.Future
+
+class EnterInspectionDateControllerSpec
+  extends ControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with BeforeAndAfterEach
+    with ScalaCheckPropertyChecks
+    with RejectedGoodsSingleJourneyTestData {
+
+  override val overrideBindings: List[GuiceableModule] =
+    List[GuiceableModule](
+      bind[AuthConnector].toInstance(mockAuthConnector),
+      bind[SessionCache].toInstance(mockSessionCache)
+    )
+
+  val controller: EnterInspectionDateController = instanceOf[EnterInspectionDateController]
+
+  implicit val messagesApi: MessagesApi = controller.messagesApi
+  implicit val messages: Messages       = MessagesImpl(Lang("en"), messagesApi)
+
+  private lazy val featureSwitch  = instanceOf[FeatureSwitchService]
+  private val messagesKey: String = "enter-inspection-date.rejected-goods"
+
+  override def beforeEach(): Unit =
+    featureSwitch.enable(Feature.RejectedGoods)
+
+  val session: SessionData = SessionData.empty.copy(
+    rejectedGoodsSingleJourney = Some(RejectedGoodsSingleJourney.empty(exampleEori))
+  )
+
+  "Enter Inspection Date Controller" must {
+
+    "not find the page if rejected goods feature is disabled" in {
+      def performAction(): Future[Result] =
+        controller.show()(FakeRequest())
+
+      featureSwitch.disable(Feature.RejectedGoods)
+
+      status(performAction()) shouldBe NOT_FOUND
+    }
+
+    "display the page" when {
+      def performAction(): Future[Result] =
+        controller.show()(FakeRequest())
+
+      "the user has not answered this question before" in {
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$messagesKey.title")
+        )
+      }
+
+      "display the page on a pre-existing journey" in {
+      }
+
+    }
+
+    "handle submit requests" when {
+      def performAction(data: (String, String)*): Future[Result] =
+        controller.submit()(FakeRequest().withFormUrlEncodedBody(data: _*))
+
+      "the user enters details for the first time" in {
+      }
+
+    }
+    
+  }
+}
