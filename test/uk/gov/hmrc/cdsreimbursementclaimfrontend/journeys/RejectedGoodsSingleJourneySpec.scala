941a6d775d5f60080c8d386b603c9c91181c34f5
Artur Opala <250927+arturopala@users.noreply.github.com>
2022-01-21 15:56:23 +0000

    [CDSR-1423][AO] add missing messages for new document types (#727)

--- test/uk/gov/hmrc/cdsreimbursementclaimfrontend/journeys/RejectedGoodsSingleJourneySpec.scala
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/journeys/RejectedGoodsSingleJourneySpec.scala
@@ -102,7 +102,6 @@ class RejectedGoodsSingleJourneySpec
         val modifiedJourney = result.getOrElse(fail(couldNotRetrieveJourney))
         modifiedJourney.isFinalized                shouldBe true
         modifiedJourney.finalizeJourneyWith("bar") shouldBe Left(JOURNEY_ALREADY_FINALIZED)
-        println(journey.prettyPrint)
       }
     }
 
