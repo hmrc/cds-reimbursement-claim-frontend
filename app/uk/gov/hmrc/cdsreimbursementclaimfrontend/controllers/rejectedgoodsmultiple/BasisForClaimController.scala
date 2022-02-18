9e6dd496911959b0f24b6594f0a0bc1e554f1021
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2022-02-18 09:31:38 +0000

    CDSR-1369: Add controller and routes

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsmultiple/BasisForClaimController.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsmultiple/BasisForClaimController.scala
@@ -69,11 +69,11 @@ class BasisForClaimController @Inject() (
         basisOfClaim =>
           (
             journey.submitBasisOfClaim(basisOfClaim),
-            Redirect(basisOfClaim match {
+            basisOfClaim match {
               case SpecialCircumstances =>
-                "/enter-special-circumstances" //FIXME: routes.EnterSpecialCircumstancesController.show()
-              case _                    => "/choose-disposal-method" //FIXME: routes.DisposalMethodController.show()
-            })
+                Redirect("/enter-special-circumstances") //FIXME: routes.EnterSpecialCircumstancesController.show()
+              case _                    => Redirect(routes.DisposalMethodController.show())
+            }
           ).asFuture
       )
   }
