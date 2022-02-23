c5ee026864cf126eea3ec28f63238c0faf2ee5e3
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2022-02-23 14:01:03 +0000

    CDSR-1432 Created a bank account type page for Multiple C&E1779 journey

---
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsmultiple/ChooseBankAccountTypeController.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.rejectedgoodsmultiple
+
+import cats.syntax.all._
+import com.google.inject.Inject
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.ViewConfig
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.{routes => baseRoutes}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.Forms.bankAccountTypeForm
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.JourneyControllerComponents
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.utils.Logging
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.{rejectedgoods => pages}
+
+import scala.concurrent.ExecutionContext
+
+class ChooseBankAccountTypeController @Inject() (
+  val jcc: JourneyControllerComponents,
+  chooseBankAccountTypePage: pages.choose_bank_account_type_page
+)(implicit val ec: ExecutionContext, viewConfig: ViewConfig)
+    extends RejectedGoodsMultipleJourneyBaseController
+    with Logging {
+
+  val show: Action[AnyContent] = actionReadJourney { implicit request => journey =>
+    Ok(
+      chooseBankAccountTypePage(
+        bankAccountTypeForm.withDefault(journey.answers.bankAccountType),
+        routes.ChooseBankAccountTypeController.submit()
+      )
+    ).asFuture
+  }
+
+  val submit: Action[AnyContent] = actionReadWriteJourney(
+    { implicit request => journey =>
+      bankAccountTypeForm
+        .bindFromRequest()
+        .fold(
+          formWithErrors =>
+            (
+              journey,
+              BadRequest(chooseBankAccountTypePage(formWithErrors, routes.ChooseBankAccountTypeController.submit()))
+            ).asFuture,
+          bankAccountType =>
+            journey
+              .submitBankAccountType(bankAccountType)
+              .map((_, Redirect("/enter-bank-account-details")))
+              .valueOr { error =>
+                logger warn error
+                (journey, Redirect(baseRoutes.IneligibleController.ineligible()))
+              }
+              .asFuture
+        )
+    },
+    fastForwardToCYAEnabled = false
+  )
+}
