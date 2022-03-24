735020afd5bed99a98b75aab9d0a730f2fbd7233
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2022-03-24 16:05:48 +0000

    CDSR-1379: Add controller and routes

---
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsscheduled/SelectDutyCodesController.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.rejectedgoodsscheduled
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import play.api.mvc.{Action, AnyContent, Call, Result}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.ViewConfig
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.JourneyControllerComponents
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.claims.SelectDutyCodesController.selectDutyCodesForm
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.DutyType
+//import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.Forms.[FORM]
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.{claims => pages}
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+@Singleton
+class SelectDutyCodesController @Inject() (
+val jcc: JourneyControllerComponents,
+selectDutyCodesPage: pages.select_duty_codes
+  )(implicit val ec: ExecutionContext, viewConfig: ViewConfig)
+  extends RejectedGoodsScheduledJourneyBaseController {
+
+  val formKey: String          = "select-duty-codes"
+  private val postAction: Call = routes.SelectDutyCodesController.submit()
+
+  val iterate: Action[AnyContent] = actionReadJourney { implicit request => _ =>
+    def selectDuties: Future[Result] = Redirect(routes.SelectDutyTypesController.show()).asFuture
+
+    def start(dutyType: DutyType): Future[Result] = Redirect(routes.SelectDutyCodesController.show(dutyType)).asFuture
+
+//    withAnswers[SelectedDutyTaxCodesReimbursementAnswer] { (_, maybeAnswer) =>
+//      maybeAnswer.flatMap(_.value.headOption).fold(selectDuties) { selectedDutyTaxCodesReimbursement =>
+//        start(selectedDutyTaxCodesReimbursement._1)
+ //     }
+   // }
+  }
+
+
+  val show: Action[AnyContent] = actionReadJourney { implicit request => journey =>
+
+  val form = selectDutyCodesForm  // .withDefault(journey.getSelectedDuties.map(_._2))
+    val dutyTypes: Seq[DutyType] = journey.getSelectedDutyTypes.getOrElse(Seq.empty)
+
+  Ok(selectDutyCodesPage(form)).asFuture
+
+  }
+
+  val submit: Action[AnyContent] = actionReadWriteJourney { implicit request => journey =>
+  Future.successful(
+  [FORM]
+  .bindFromRequest()
+  .fold(
+  formWithErrors =>
+  (
+  journey,
+  BadRequest(
+    selectDutyCodesPage(
+  formWithErrors,
+  postAction
+  )
+  )
+  ),
+    dutyCodes =>
+  (
+  journey.[SUBMIT METHOD](dutyCodes),
+  Redirect("routes.[NEW CONTROLLER].show()") //FIXME: routes.[NEW CONTROLLER].show()
+  )
+  )
+  )
+  }
+
+  }
