f1fbead5fdf2177ccf24c16f910d7d86ff1b3fca
Steve Edmans <30315073+steve-edmans@users.noreply.github.com>
2022-04-01 10:00:38 +0100

    CDSR-1372: Enter Rejected Goods Details

---
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsscheduled/EnterRejectedGoodsDetailsControllerSpec.scala
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.rejectedgoodsscheduled
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.i18n.Lang
+import play.api.i18n.Messages
+import play.api.i18n.MessagesApi
+import play.api.i18n.MessagesImpl
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.cache.SessionCache
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.AuthSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.ControllerSpec
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.SessionSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.RejectedGoodsScheduledJourneyTestData
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.Feature
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.SessionData
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.generators.Generators.alphaNumGenerator
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.generators.genStringWithMaxSizeOfN
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.services.FeatureSwitchService
+import scala.concurrent.Future
+
+class EnterRejectedGoodsDetailsControllerSpec
+    extends ControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with BeforeAndAfterEach
+    with ScalaCheckPropertyChecks
+    with RejectedGoodsScheduledJourneyTestData {
+
+  override val overrideBindings: List[GuiceableModule] =
+    List[GuiceableModule](
+      bind[AuthConnector].toInstance(mockAuthConnector),
+      bind[SessionCache].toInstance(mockSessionCache)
+    )
+
+  private val session = SessionData(emptyJourney)
+
+  val controller: EnterRejectedGoodsDetailsController = instanceOf[EnterRejectedGoodsDetailsController]
+
+  val formKey: String = "enter-rejected-goods-details.rejected-goods"
+
+  implicit val messagesApi: MessagesApi = controller.messagesApi
+  implicit val messages: Messages       = MessagesImpl(Lang("en"), messagesApi)
+
+  private lazy val featureSwitch = instanceOf[FeatureSwitchService]
+
+  override def beforeEach(): Unit = featureSwitch enable Feature.RejectedGoods
+
+  "Enter Rejected Goods Details Controller" should {
+
+    "show page" when {
+      def performAction(): Future[Result] =
+        controller.show()(FakeRequest())
+
+      "do not find the page if rejected goods feature is disabled" in {
+        featureSwitch.disable(Feature.RejectedGoods)
+
+        status(performAction()) shouldBe NOT_FOUND
+      }
+
+      "display the page on a new journey" in {
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$formKey.title"),
+          doc => {
+            formAction(doc)           shouldBe routes.EnterRejectedGoodsDetailsController.submit().url
+            selectedTextArea(doc).get shouldBe ""
+          }
+        )
+      }
+
+      "display the page on a pre-existing journey" in forAll(genStringWithMaxSizeOfN(50)) { details =>
+        val journey = session.rejectedGoodsScheduledJourney.get.submitDetailsOfRejectedGoods(details)
+
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey))
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$formKey.title"),
+          doc => {
+            formAction(doc)           shouldBe routes.EnterRejectedGoodsDetailsController.submit().url
+            selectedTextArea(doc).get shouldBe details
+          }
+        )
+      }
+    }
+
+    "submit page" when {
+      def performAction(data: (String, String)*): Future[Result] =
+        controller.submit()(FakeRequest().withFormUrlEncodedBody(data: _*))
+
+      "do not find the page if rejected goods feature is disabled" in {
+        featureSwitch.disable(Feature.RejectedGoods)
+
+        status(performAction()) shouldBe NOT_FOUND
+      }
+
+      "reject an empty form" in {
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$formKey.title"),
+          doc => getErrorSummary(doc) shouldBe messageFromMessageKey(s"$formKey.error.required"),
+          expectedStatus = BAD_REQUEST
+        )
+      }
+
+      "reject an too large an amount of text" in forAll(alphaNumGenerator(600)) { details =>
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(formKey -> details),
+          messageFromMessageKey(s"$formKey.title"),
+          doc => getErrorSummary(doc) shouldBe messageFromMessageKey(s"$formKey.error.maxLength"),
+          expectedStatus = BAD_REQUEST
+        )
+      }
+
+      "accept less than 500 characters text" in forAll(genStringWithMaxSizeOfN(50)) { details =>
+        val updatedJourney = session.rejectedGoodsScheduledJourney.get.submitDetailsOfRejectedGoods(details)
+        val updatedSession = SessionData(updatedJourney)
+
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+          mockStoreSession(updatedSession)(Right(()))
+        }
+
+        checkIsRedirect(
+          performAction(formKey -> details),
+          routes.SelectDutyTypesController.show()
+        )
+      }
+    }
+  }
+}
