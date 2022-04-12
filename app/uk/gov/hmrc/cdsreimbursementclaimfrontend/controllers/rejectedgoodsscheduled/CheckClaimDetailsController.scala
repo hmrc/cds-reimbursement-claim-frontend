6c85b017752823b3955b80734a6d08bcb514e758
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2022-04-12 12:12:53 +0100

    CDSDR-1381: Add answer to controller

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsscheduled/CheckClaimDetailsController.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsscheduled/CheckClaimDetailsController.scala
@@ -1,17 +1,34 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.rejectedgoodsscheduled
-
-class CheckClaimDetailsController {
-
-}
 
 import play.api.data.Form
 import play.api.mvc.Action
 import play.api.mvc.AnyContent
 import play.api.mvc.Call
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.ViewConfig
-import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.{Forms, JourneyControllerComponents, YesOrNoQuestionForm}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.Forms
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.JourneyControllerComponents
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.YesOrNoQuestionForm
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.rejectedgoodsscheduled.CheckClaimDetailsController
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.rejectedgoodsscheduled.CheckClaimDetailsController.checkClaimDetailsKey
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.DutyType
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.Reimbursement
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.TaxCode
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.answers.YesNo
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.answers.YesNo.No
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.answers.YesNo.Yes
@@ -19,27 +36,29 @@ import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.{rejectedgoods => pa
 
 import javax.inject.Inject
 import javax.inject.Singleton
+import scala.collection.immutable.SortedMap
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
 @Singleton
 class CheckClaimDetailsController @Inject() (
-val jcc: JourneyControllerComponents,
-checkClaimDetailsPage: pages.check-claim-details-scheduled
-  )(implicit val ec: ExecutionContext, viewConfig: ViewConfig)
-  extends RejectedGoodsScheduledJourneyBaseController {
+  val jcc: JourneyControllerComponents,
+  checkClaimDetailsPage: pages.check - claim - details - scheduled
+)(implicit val ec: ExecutionContext, viewConfig: ViewConfig)
+    extends RejectedGoodsScheduledJourneyBaseController {
 
   val checkClaimDetailsForm: Form[YesNo] = YesOrNoQuestionForm(CheckClaimDetailsController.checkClaimDetailsKey)
 
-  private val postAction: Call = routes.SelectDutyTypesController.submit() //FIXME: routes.CheckClaimDetailsController.submit()
+  private val postAction: Call =
+    routes.SelectDutyTypesController.submit() //FIXME: routes.CheckClaimDetailsController.submit()
 
   val show: Action[AnyContent] = actionReadJourney { implicit request => journey =>
-
-  Ok(checkClaimDetailsPage(checkClaimDetailsForm, postAction)).asFuture
+    val answers: SortedMap[DutyType, SortedMap[TaxCode, Reimbursement]] = journey.getReimbursementClaims
+    Ok(checkClaimDetailsPage(answers, checkClaimDetailsForm, postAction)).asFuture
 
   }
 
- //val submit: Action[AnyContent] = actionReadWriteJourney { implicit request => journey =>
+  //val submit: Action[AnyContent] = actionReadWriteJourney { implicit request => journey =>
 //  Future.successful(
 //    checkClaimDetailsForm
 //  .bindFromRequest()
@@ -63,7 +82,7 @@ checkClaimDetailsPage: pages.check-claim-details-scheduled
 //  )
 //}
 
-  }
+}
 
 object CheckClaimDetailsController {
   val checkClaimDetailsKey: String = "check-claim-summary"
