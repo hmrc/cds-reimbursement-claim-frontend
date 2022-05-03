af0b7fdddd0f5809a20454e8bc4adbfe4db4aec7
shivalasvegas <67016203+shivalasvegas@users.noreply.github.com>
2022-05-03 11:08:58 +0100

    CDSR-1648: remove commented code

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/views/components/summary/InspectionDateAndAddressSummary.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/views/components/summary/InspectionDateAndAddressSummary.scala
@@ -91,7 +91,6 @@ object InspectionDateAndAddressSummary {
       Seq(
         SummaryListRow(
           key = Key(Text(messages(s"$key.inspection-date"))),
-          //value = Value(Text(inspectionDate.value.toString)),
           value = Value(Text(InspectionDate(inspectionDate.value).checkYourDetailsDisplayFormat)),
           actions = Some(changeInspectionDateAction)
         ),
