891bec6e878211c65f827b52835e8ca32137d161
Tony Merryfield (Razer Blade 15) <tony.merryfield@oddbunch.co.uk>
2022-05-11 13:20:14 +0100

    CDSR-1686 - Fix broken test

--- test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/claims/ChooseClaimTypeControllerSpec.scala
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/claims/ChooseClaimTypeControllerSpec.scala
@@ -172,12 +172,7 @@ class ChooseClaimTypeControllerSpec extends ControllerSpec with AuthSupport with
         checkPageIsDisplayed(
           performAction(Seq(dataKey -> "HACKED_VALUE")),
           messageFromMessageKey(s"$formKey.title"),
-          doc =>
-            extractErrorMessage(
-              doc
-            ) shouldBe "<span class=\"govuk-visually-hidden\">Error:</span> " + messageFromMessageKey(
-              s"$dataKey.error.required"
-            ),
+          doc => extractErrorMessage(doc) shouldBe "Error: " + messageFromMessageKey(s"$dataKey.error.required"),
           expectedStatus = BAD_REQUEST
         )
 
@@ -187,7 +182,7 @@ class ChooseClaimTypeControllerSpec extends ControllerSpec with AuthSupport with
   }
 
   private def extractErrorMessage(doc: Document): String =
-    doc.select(".govuk-error-message").html()
+    doc.select(".govuk-error-message").text()
 
   private def radioButtons(doc: Document): Elements =
     doc.select(s"div.govuk-radios div.govuk-radios__item")
