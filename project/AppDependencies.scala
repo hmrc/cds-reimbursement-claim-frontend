0ee20134a03d8bcdd63a9fad3bf0feb61718f697
Artur Opala <250927+arturopala@users.noreply.github.com>
2022-06-07 11:53:22 +0100

    update frontend library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-28" % "5.3.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc"         % "3.20.0-play-28",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc"         % "3.21.0-play-28",
     "uk.gov.hmrc"           %% "mongo-caching"              % "7.0.0-play-28",
     "uk.gov.hmrc"           %% "play-language"              % "4.13.0-play-28",
     "org.typelevel"         %% "cats-core"                  % "2.7.0",
