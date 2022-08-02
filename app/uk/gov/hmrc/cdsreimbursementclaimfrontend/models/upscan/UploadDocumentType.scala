bdb511d117699e142b46fc4e361c56147f4e2389
Artur Opala <250927+arturopala@users.noreply.github.com>
2022-08-02 14:10:07 +0100

    [CDSR-1561][CDSR-1798] Securities | /choose-file-type; Securities | US1:AC10 Missing Document upload CONNECT TO UDF

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/models/upscan/UploadDocumentType.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/models/upscan/UploadDocumentType.scala
@@ -17,6 +17,7 @@
 package uk.gov.hmrc.cdsreimbursementclaimfrontend.models.upscan
 
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.utils.EnumerationFormat
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.ReasonForSecurity
 
 sealed trait UploadDocumentType
 
@@ -26,17 +27,29 @@ object UploadDocumentType extends EnumerationFormat[UploadDocumentType] {
   case object BillOfLading extends UploadDocumentType
   case object CommercialInvoice extends UploadDocumentType
   case object CorrespondenceTrader extends UploadDocumentType
+  case object ImportDeclaration extends UploadDocumentType
+  case object ExportDeclaration extends UploadDocumentType
   case object ImportAndExportDeclaration extends UploadDocumentType
   case object PackingList extends UploadDocumentType
+  case object ExportPackingList extends UploadDocumentType
+  case object ImportPackingList extends UploadDocumentType
   case object ProofOfAuthority extends UploadDocumentType
+  case object ProofOfEligibility extends UploadDocumentType
+  case object ProofOfOrigin extends UploadDocumentType
   case object SubstituteEntry extends UploadDocumentType
+  case object SubstituteOrDiversionEntry extends UploadDocumentType
   case object Other extends UploadDocumentType
   case object ScheduleOfMRNs extends UploadDocumentType
   case object CalculationWorksheet extends UploadDocumentType
+  case object CalculationWorksheetOrFinalSalesFigures extends UploadDocumentType
   case object DocumentaryProofFaultyOrNotWhatOrdered extends UploadDocumentType
   case object ProofOfExportOrDestruction extends UploadDocumentType
   case object AdditionalSupportingDocuments extends UploadDocumentType
   case object LetterOfAuthority extends UploadDocumentType
+  case object SupportingEvidence extends UploadDocumentType
+  case object BillOfDischarge3 extends UploadDocumentType
+  case object BillOfDischarge4 extends UploadDocumentType
+  case object QuotaLicense extends UploadDocumentType
 
   override val values: Set[UploadDocumentType] =
     Set[UploadDocumentType](
@@ -44,17 +57,29 @@ object UploadDocumentType extends EnumerationFormat[UploadDocumentType] {
       BillOfLading,
       CommercialInvoice,
       CorrespondenceTrader,
+      ImportDeclaration,
+      ExportDeclaration,
       ImportAndExportDeclaration,
       PackingList,
+      ExportPackingList,
+      ImportPackingList,
       ProofOfAuthority,
+      ProofOfEligibility,
+      ProofOfOrigin,
       SubstituteEntry,
+      SubstituteOrDiversionEntry,
       ScheduleOfMRNs,
       Other,
       CalculationWorksheet,
+      CalculationWorksheetOrFinalSalesFigures,
       DocumentaryProofFaultyOrNotWhatOrdered,
       ProofOfExportOrDestruction,
       AdditionalSupportingDocuments,
-      LetterOfAuthority
+      LetterOfAuthority,
+      SupportingEvidence,
+      BillOfDischarge3,
+      BillOfDischarge4,
+      QuotaLicense
     )
 
   val c285EvidenceTypes: Seq[UploadDocumentType] =
@@ -87,4 +112,74 @@ object UploadDocumentType extends EnumerationFormat[UploadDocumentType] {
 
   val rejectedGoodsScheduledTypes: Seq[UploadDocumentType] =
     rejectedGoodsSingleTypes
+
+  val securitiesTypes: ReasonForSecurity => Option[Seq[UploadDocumentType]] = {
+    val pf: PartialFunction[ReasonForSecurity, Seq[UploadDocumentType]] = {
+      case ReasonForSecurity.InwardProcessingRelief =>
+        Seq[UploadDocumentType](
+          ImportDeclaration,
+          ExportDeclaration,
+          ExportPackingList,
+          SubstituteOrDiversionEntry,
+          BillOfDischarge3,
+          Other
+        )
+
+      case ReasonForSecurity.EndUseRelief =>
+        Seq[UploadDocumentType](
+          ImportDeclaration,
+          ExportDeclaration,
+          ExportPackingList,
+          SubstituteOrDiversionEntry,
+          BillOfDischarge4,
+          Other
+        )
+
+      case ReasonForSecurity.OutwardProcessingRelief | ReasonForSecurity.TemporaryAdmission2M |
+          ReasonForSecurity.TemporaryAdmission3M | ReasonForSecurity.TemporaryAdmission6M |
+          ReasonForSecurity.TemporaryAdmission2Y =>
+        Seq[UploadDocumentType](
+          CommercialInvoice,
+          ImportDeclaration,
+          ExportDeclaration,
+          ImportPackingList,
+          ExportPackingList,
+          SubstituteOrDiversionEntry,
+          Other
+        )
+
+      case ReasonForSecurity.AccountSales =>
+        Seq[UploadDocumentType](
+          CommercialInvoice,
+          ImportDeclaration,
+          CalculationWorksheetOrFinalSalesFigures,
+          Other
+        )
+
+      case ReasonForSecurity.MissingPreferenceCertificate =>
+        Seq[UploadDocumentType](
+          CommercialInvoice,
+          ImportDeclaration,
+          ProofOfOrigin,
+          Other
+        )
+
+      case ReasonForSecurity.MissingLicenseQuota =>
+        Seq[UploadDocumentType](
+          CommercialInvoice,
+          ImportDeclaration,
+          QuotaLicense,
+          Other
+        )
+
+      case ReasonForSecurity.CommunitySystemsOfDutyRelief =>
+        Seq[UploadDocumentType](
+          CommercialInvoice,
+          ImportDeclaration,
+          ProofOfEligibility,
+          Other
+        )
+    }
+    pf.lift
+  }
 }
---
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/securities/ChooseFileTypeControllerSpec.scala
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.securities
+
+import org.jsoup.nodes.Document
+import org.scalatest.BeforeAndAfterEach
+import play.api.i18n.Lang
+import play.api.i18n.Messages
+import play.api.i18n.MessagesApi
+import play.api.i18n.MessagesImpl
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.cache.SessionCache
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.AuthSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.PropertyBasedControllerSpec
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.SessionSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.SecuritiesJourneyGenerators._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.Feature
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.SessionData
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.services.FeatureSwitchService
+
+import scala.concurrent.Future
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.SecuritiesJourney
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.upscan.UploadDocumentType
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.support.TestWithJourneyGenerator
+
+class ChooseFileTypeControllerSpec
+    extends PropertyBasedControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with BeforeAndAfterEach
+    with TestWithJourneyGenerator[SecuritiesJourney] {
+
+  override val overrideBindings: List[GuiceableModule] =
+    List[GuiceableModule](
+      bind[AuthConnector].toInstance(mockAuthConnector),
+      bind[SessionCache].toInstance(mockSessionCache)
+    )
+
+  val controller: ChooseFileTypeController = instanceOf[ChooseFileTypeController]
+
+  implicit val messagesApi: MessagesApi = controller.messagesApi
+  implicit val messages: Messages       = MessagesImpl(Lang("en"), messagesApi)
+
+  private lazy val featureSwitch = instanceOf[FeatureSwitchService]
+
+  private val messagesKey: String = "choose-file-type"
+
+  override def beforeEach(): Unit =
+    featureSwitch.enable(Feature.Securities)
+
+  def validateChooseFileTypePage(doc: Document, journey: SecuritiesJourney) = {
+    val expectedDocumentTypes = journey.getDocumentTypesIfRequired.get
+      .map(dt => (messages(s"choose-file-type.file-type.$dt"), UploadDocumentType.keyOf(dt)))
+
+    radioItems(doc) should contain theSameElementsAs expectedDocumentTypes
+    hasContinueButton(doc)
+  }
+
+  "ChooseFileTypeController" when {
+
+    "Show page" must {
+
+      def performAction(): Future[Result] = controller.show()(FakeRequest())
+
+      "not find the page if securities feature is disabled" in {
+        featureSwitch.disable(Feature.Securities)
+        status(performAction()) shouldBe NOT_FOUND
+      }
+
+      "display the page" in forAllWith(
+        JourneyGenerator(
+          testParamsGenerator = mrnWithRfsRequiringDocumentTypeWithDisplayDeclarationGen,
+          journeyBuilder = buildSecuritiesJourneyWithSomeSecuritiesSelected
+        )
+      ) { case (journey, _) =>
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey))
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$messagesKey.securities.title"),
+          doc => validateChooseFileTypePage(doc, journey)
+        )
+
+      }
+
+      "display the page when in change mode" in {
+        forAll(completeJourneyGen) { journey =>
+          whenever(journey.requiresDocumentTypeSelection) {
+            inSequence {
+              mockAuthWithNoRetrievals()
+              mockGetSession(SessionData(journey))
+            }
+
+            checkPageIsDisplayed(
+              performAction(),
+              messageFromMessageKey(s"$messagesKey.securities.title"),
+              doc => validateChooseFileTypePage(doc, journey)
+            )
+          }
+        }
+      }
+    }
+
+    "submitted the document type" must {
+
+      def performAction(data: (String, String)*): Future[Result] =
+        controller.submit()(FakeRequest().withFormUrlEncodedBody(data: _*))
+
+      "not succeed if securities feature is disabled" in {
+        featureSwitch.disable(Feature.Securities)
+        status(performAction(("choose-file-type", "Other"))) shouldBe NOT_FOUND
+      }
+
+      "redirect to choose files when valid document type selection" in forAllWith(
+        JourneyGenerator(
+          testParamsGenerator = mrnWithRfsRequiringDocumentTypeWithDisplayDeclarationGen,
+          journeyBuilder = buildSecuritiesJourneyWithSomeSecuritiesSelected
+        )
+      ) { case (journey, _) =>
+        journey.getDocumentTypesIfRequired.get.foreach { documentType =>
+          inSequence {
+            mockAuthWithNoRetrievals()
+            mockGetSession(SessionData(journey))
+            mockStoreSession(
+              SessionData(journey.submitDocumentTypeSelection(documentType).getOrFail)
+            )(Right(()))
+          }
+          checkIsRedirect(
+            performAction("choose-file-type" -> s"$documentType"),
+            routes.UploadFilesController.show()
+          )
+        }
+      }
+
+      "re-display the page when invalid document type selection" in forSomeWith(
+        JourneyGenerator(
+          testParamsGenerator = mrnWithRfsRequiringDocumentTypeWithDisplayDeclarationGen,
+          journeyBuilder = buildSecuritiesJourneyWithSomeSecuritiesSelected
+        )
+      ) { case (journey, _) =>
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey))
+        }
+        checkPageIsDisplayed(
+          performAction("choose-file-type" -> "Foo"),
+          messageFromMessageKey(s"$messagesKey.securities.title"),
+          doc => validateChooseFileTypePage(doc, journey),
+          expectedStatus = 400
+        )
+      }
+
+      "re-display the page when unsupported document type selection" in forSomeWith(
+        JourneyGenerator(
+          testParamsGenerator = mrnWithRfsRequiringDocumentTypeWithDisplayDeclarationGen,
+          journeyBuilder = buildSecuritiesJourneyWithSomeSecuritiesSelected
+        )
+      ) { case (journey, _) =>
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey))
+        }
+        checkPageIsDisplayed(
+          performAction("choose-file-type" -> "AirWayBill"),
+          messageFromMessageKey(s"$messagesKey.securities.title"),
+          doc => validateChooseFileTypePage(doc, journey),
+          expectedStatus = 400
+        )
+      }
+
+      "re-display the page when nothing has been selected" in forSomeWith(
+        JourneyGenerator(
+          testParamsGenerator = mrnWithRfsRequiringDocumentTypeWithDisplayDeclarationGen,
+          journeyBuilder = buildSecuritiesJourneyWithSomeSecuritiesSelected
+        )
+      ) { case (journey, _) =>
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey))
+        }
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$messagesKey.securities.title"),
+          doc => validateChooseFileTypePage(doc, journey),
+          expectedStatus = 400
+        )
+      }
+
+    }
+
+  }
+
+}
