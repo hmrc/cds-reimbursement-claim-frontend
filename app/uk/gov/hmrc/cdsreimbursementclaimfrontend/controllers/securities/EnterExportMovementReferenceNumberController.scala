72340c098e33f2481c4d4aee4a50049e04c3dac7
Jamal (Abdulmajid) Osman <abdul.majid.ahmed.osman@digital.hmrc.gov.uk>
2022-08-24 16:35:24 +0100

    CDSR-1555, CDSR-1556: * add redirect guard logic to export-method and enter-export-mrn pages.

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/securities/EnterExportMovementReferenceNumberController.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/securities/EnterExportMovementReferenceNumberController.scala
@@ -16,6 +16,8 @@
 
 package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.securities
 
+import cats.implicits.catsSyntaxEq
+import cats.implicits.catsSyntaxTuple2Semigroupal
 import com.github.arturopala.validator.Validator.Validate
 import com.google.inject.Inject
 import com.google.inject.Singleton
@@ -25,7 +27,9 @@ import play.api.data.Forms.mapping
 import play.api.data.Forms.nonEmptyText
 import play.api.mvc.Action
 import play.api.mvc.AnyContent
+import play.api.mvc.Request
 import play.api.mvc.Result
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.ErrorHandler
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.ViewConfig
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.JourneyControllerComponents
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.securities.EnterExportMovementReferenceNumberController.enterExportMovementReferenceNumberKey
@@ -33,18 +37,23 @@ import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.securities.EnterExp
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.SecuritiesJourney
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.SecuritiesJourney.Checks.declarantOrImporterEoriMatchesUserOrHasBeenVerified
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.SecuritiesJourney.Checks.hasMRNAndDisplayDeclarationAndRfS
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.ReasonForSecurity.temporaryAdmissions
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.TemporaryAdmissionMethodOfDisposal.ExportedInSingleShipment
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.ReasonForSecurity
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.TemporaryAdmissionMethodOfDisposal
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.ids.MRN
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.services.ClaimService
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.securities.enter_export_movement_reference_number
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 
 @Singleton
 class EnterExportMovementReferenceNumberController @Inject() (
   val jcc: JourneyControllerComponents,
   claimService: ClaimService,
   enterExportMovementReferenceNumberPage: enter_export_movement_reference_number
-)(implicit viewConfig: ViewConfig, ec: ExecutionContext)
+)(implicit viewConfig: ViewConfig, errorHandler: ErrorHandler, ec: ExecutionContext)
     extends SecuritiesJourneyBaseController {
 
   final override val actionPrecondition: Option[Validate[SecuritiesJourney]] =
@@ -52,48 +61,56 @@ class EnterExportMovementReferenceNumberController @Inject() (
       hasMRNAndDisplayDeclarationAndRfS &
         declarantOrImporterEoriMatchesUserOrHasBeenVerified
     )
-  def show: Action[AnyContent]                                               = actionReadJourney { implicit request => journey =>
-    Ok(
-      enterExportMovementReferenceNumberPage(
-        exportMovementReferenceNumberForm,
-        routes.EnterExportMovementReferenceNumberController.submit()
-      )
-    ).asFuture
+
+  def show: Action[AnyContent] = actionReadWriteJourney { implicit request => journey =>
+    whenTemporaryAdmissionExportedInSingleShipment(journey) {
+      (
+        journey,
+        Ok(
+          enterExportMovementReferenceNumberPage(
+            exportMovementReferenceNumberForm,
+            routes.EnterExportMovementReferenceNumberController.submit()
+          )
+        )
+      ).asFuture
+    }
   }
 
   def submit: Action[AnyContent] = actionReadWriteJourney { implicit request => journey =>
-    exportMovementReferenceNumberForm.bindFromRequest
-      .fold(
-        formWithErrors =>
-          (
-            journey,
-            BadRequest(
-              enterExportMovementReferenceNumberPage(
-                formWithErrors.copy(data = Map.empty),
-                routes.EnterExportMovementReferenceNumberController.submit()
+    whenTemporaryAdmissionExportedInSingleShipment(journey) {
+      exportMovementReferenceNumberForm.bindFromRequest
+        .fold(
+          formWithErrors =>
+            (
+              journey,
+              BadRequest(
+                enterExportMovementReferenceNumberPage(
+                  formWithErrors.copy(data = Map.empty),
+                  routes.EnterExportMovementReferenceNumberController.submit()
+                )
               )
-            )
-          ).asFuture,
-        mrn =>
-          claimService
-            .getDisplayDeclaration(mrn)
-            .fold(
-              _ => submitMrnAndContinue(mrn, journey),
-              _ =>
-                (
-                  journey,
-                  BadRequest(
-                    enterExportMovementReferenceNumberPage(
-                      exportMovementReferenceNumberForm.copy(
-                        data = Map.empty,
-                        errors = List(FormError(enterExportMovementReferenceNumberKey, "securities.error.import"))
-                      ),
-                      routes.EnterExportMovementReferenceNumberController.submit()
+            ).asFuture,
+          mrn =>
+            claimService
+              .getDisplayDeclaration(mrn)
+              .fold(
+                _ => submitMrnAndContinue(mrn, journey),
+                _ =>
+                  (
+                    journey,
+                    BadRequest(
+                      enterExportMovementReferenceNumberPage(
+                        exportMovementReferenceNumberForm.copy(
+                          data = Map.empty,
+                          errors = List(FormError(enterExportMovementReferenceNumberKey, "securities.error.import"))
+                        ),
+                        routes.EnterExportMovementReferenceNumberController.submit()
+                      )
                     )
                   )
-                )
-            )
-      )
+              )
+        )
+    }
   }
 
   private def submitMrnAndContinue(mrn: MRN, journey: SecuritiesJourney): (SecuritiesJourney, Result) =
@@ -103,6 +120,21 @@ class EnterExportMovementReferenceNumberController @Inject() (
         _ => (journey, Redirect(routes.CheckClaimantDetailsController.show())),
         (_, Redirect(routes.CheckClaimantDetailsController.show()))
       )
+
+  def whenTemporaryAdmissionExportedInSingleShipment(
+    journey: SecuritiesJourney
+  )(body: => Future[(SecuritiesJourney, Result)])(implicit request: Request[_]): Future[(SecuritiesJourney, Result)] =
+    (journey.getReasonForSecurity, journey.answers.temporaryAdmissionMethodOfDisposal) match {
+      case (None, _)                                                                                       =>
+        (journey, errorHandler.errorResult()).asFuture
+      case (Some(rfs), _) if !temporaryAdmissions.contains(rfs)                                            =>
+        (journey, Redirect(routes.CheckClaimantDetailsController.show)).asFuture
+      case (Some(rfs), Some(mod)) if temporaryAdmissions.contains(rfs) && mod =!= ExportedInSingleShipment =>
+        (journey, Redirect(routes.CheckClaimantDetailsController.show)).asFuture
+      case (Some(rfs), None) if temporaryAdmissions.contains(rfs)                                          =>
+        (journey, Redirect(routes.ChooseExportMethodController.show)).asFuture
+      case (Some(rfs), Some(ExportedInSingleShipment)) if temporaryAdmissions.contains(rfs)                => body
+    }
 }
 
 object EnterExportMovementReferenceNumberController {
