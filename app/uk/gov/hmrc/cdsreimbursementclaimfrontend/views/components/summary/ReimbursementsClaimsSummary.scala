7dc9cff7d709e740a359196f3cddc6df2038724b
Haseem Isaac <haseem@tasheel.technology>
2022-09-14 17:03:11 +0100

    CDSR-1912: Fix import

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/views/components/summary/ReimbursementsClaimsSummary.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/views/components/summary/ReimbursementsClaimsSummary.scala
@@ -18,7 +18,12 @@ package uk.gov.hmrc.cdsreimbursementclaimfrontend.views.components.summary
 
 import play.api.i18n.Messages
 import play.api.mvc.Call
-import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.{AmountPaidWithCorrect, BigDecimalOps, DutyType, OrdinalNumber, TaxCode, TaxCodes}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.AmountPaidWithCorrect
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.BigDecimalOps
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.DutyType
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.OrdinalNumber
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.TaxCode
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.TaxCodes
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.ids.MRN
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
