575c47b1116cdb0a5be7d445e72a3b35f399fdad
Artur Opala <250927+arturopala@users.noreply.github.com>
2023-02-10 08:32:48 -0500

    [CDSR-2122] add SelectDutiesController to overpayments multiple v2 journey

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/overpaymentsmultiple_v2/SelectDutiesController.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/overpaymentsmultiple_v2/SelectDutiesController.scala
@@ -16,17 +16,108 @@
 
 package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.overpaymentsmultiple_v2
 
-import com.google.inject.Inject
-import com.google.inject.Singleton
+import com.github.arturopala.validator.Validator.Validate
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.ViewConfig
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.Forms.selectDutiesForm
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.JourneyControllerComponents
-import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.mixins.WorkInProgressMixin
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.MRNMultipleRoutes
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.{routes => baseRoutes}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.OverpaymentsMultipleJourney
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.OverpaymentsMultipleJourney.Checks._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.TaxCode
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.claims.mrn_does_not_exist
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.common.select_duties
 
+import javax.inject.Inject
+import javax.inject.Singleton
 import scala.concurrent.ExecutionContext
 
 @Singleton
 class SelectDutiesController @Inject() (
-  val jcc: JourneyControllerComponents
-)(implicit val viewConfig: ViewConfig, val ec: ExecutionContext)
-    extends OverpaymentsMultipleJourneyBaseController
-    with WorkInProgressMixin {}
+  val jcc: JourneyControllerComponents,
+  selectDutiesPage: select_duties,
+  mrnDoesNotExistPage: mrn_does_not_exist
+)(implicit val ec: ExecutionContext, val viewConfig: ViewConfig)
+    extends OverpaymentsMultipleJourneyBaseController {
+
+  // Allow actions only if the MRN and ACC14 declaration are in place, and the EORI has been verified.
+  final override val actionPrecondition: Option[Validate[OverpaymentsMultipleJourney]] =
+    Some(hasMRNAndDisplayDeclaration & declarantOrImporterEoriMatchesUserOrHasBeenVerified)
+
+  val showFirst: Action[AnyContent] = show(1)
+
+  def show(pageIndex: Int): Action[AnyContent] = actionReadJourney { implicit request => journey =>
+    journey
+      .getNthMovementReferenceNumber(pageIndex - 1)
+      .fold(BadRequest(mrnDoesNotExistPage())) { mrn =>
+        val availableDuties: Seq[(TaxCode, Boolean)] = journey.getAvailableDuties(mrn)
+        if (availableDuties.isEmpty) {
+          logger.warn("No available duties")
+          Redirect(baseRoutes.IneligibleController.ineligible())
+        } else {
+          val form = selectDutiesForm(availableDuties.map(_._1)).withDefault(journey.getSelectedDuties(mrn))
+          Ok(
+            selectDutiesPage(
+              form,
+              availableDuties,
+              Some((pageIndex, mrn)),
+              false,
+              MRNMultipleRoutes.subKey,
+              routes.SelectDutiesController.submit(pageIndex)
+            )
+          )
+        }
+      }
+      .asFuture
+  }
+
+  def submit(pageIndex: Int): Action[AnyContent] = actionReadWriteJourney(
+    { implicit request => journey =>
+      journey
+        .getNthMovementReferenceNumber(pageIndex - 1)
+        .fold((journey, BadRequest(mrnDoesNotExistPage()))) { mrn =>
+          val availableDuties: Seq[(TaxCode, Boolean)] = journey.getAvailableDuties(mrn)
+          if (availableDuties.isEmpty) {
+            logger.warn("No available duties")
+            (journey, Redirect(baseRoutes.IneligibleController.ineligible()))
+          } else {
+            val form = selectDutiesForm(availableDuties.map(_._1))
+            form
+              .bindFromRequest()
+              .fold(
+                formWithErrors =>
+                  (
+                    journey,
+                    BadRequest(
+                      selectDutiesPage(
+                        formWithErrors,
+                        availableDuties,
+                        Some((pageIndex, mrn)),
+                        false,
+                        MRNMultipleRoutes.subKey,
+                        routes.SelectDutiesController.submit(pageIndex)
+                      )
+                    )
+                  ),
+                taxCodesSelected =>
+                  (
+                    journey
+                      .selectAndReplaceTaxCodeSetForReimbursement(mrn, taxCodesSelected)
+                      .getOrElse(journey),
+                    Redirect(
+                      taxCodesSelected.headOption match {
+                        case Some(taxCode) => routes.EnterClaimController.show(pageIndex, taxCode)
+                        case None          => routes.SelectDutiesController.show(pageIndex)
+                      }
+                    )
+                  )
+              )
+          }
+        }
+        .asFuture
+    },
+    fastForwardToCYAEnabled = false
+  )
+}
