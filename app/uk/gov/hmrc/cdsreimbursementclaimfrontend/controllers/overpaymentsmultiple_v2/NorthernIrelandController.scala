a02d865f4477a563a88acec9a7f0a844d7ef9048
Robert Udah <airudah2000@gmail.com>
2023-02-13 10:48:52 +0000

    Initial changes. WIP

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/overpaymentsmultiple_v2/NorthernIrelandController.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/overpaymentsmultiple_v2/NorthernIrelandController.scala
@@ -16,17 +16,88 @@
 
 package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.overpaymentsmultiple_v2
 
+import cats.data.EitherT
 import com.google.inject.Inject
 import com.google.inject.Singleton
-import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.ViewConfig
-import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.JourneyControllerComponents
-import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.mixins.WorkInProgressMixin
+import play.api.mvc._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.cache.SessionCache
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.{ErrorHandler, ViewConfig}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.Forms.northernIrelandForm
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.{JourneyBindable, JourneyControllerComponents, SessionDataExtractor, SessionUpdates}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.actions.{AuthenticatedAction, RequestWithSessionData, SessionDataAction, WithAuthAndSessionDataAction}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.claims.OverpaymentsRoutes
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.{DraftClaim, Error}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.JourneyStatus.FillingOutClaim.from
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.answers.YesNo
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.answers.YesNo.{No, Yes}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.utils.Logging
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.overpayments.claim_northern_ireland
 
 import scala.concurrent.ExecutionContext
 
 @Singleton
 class NorthernIrelandController @Inject() (
-  val jcc: JourneyControllerComponents
-)(implicit val viewConfig: ViewConfig, val ec: ExecutionContext)
-    extends OverpaymentsMultipleJourneyBaseController
-    with WorkInProgressMixin {}
+  val jcc: JourneyControllerComponents,
+  val authenticatedAction: AuthenticatedAction,
+  val sessionDataAction: SessionDataAction,
+  sessionStore: SessionCache,
+  northernIrelandAnswerPage: claim_northern_ireland
+)(implicit val viewConfig: ViewConfig, val ec: ExecutionContext, errorHandler: ErrorHandler)
+  extends OverpaymentsMultipleJourneyBaseController
+    with WithAuthAndSessionDataAction
+    with SessionUpdates
+    with SessionDataExtractor
+    with Logging {
+
+
+  implicit val journey: JourneyBindable                   = JourneyBindable.Multiple
+  implicit val dataExtractor: DraftClaim => Option[YesNo] = _.whetherNorthernIrelandAnswer
+  private val postAction: Call                            = OverpaymentsRoutes.NorthernIrelandController.submit(journey)
+
+  val show: Action[AnyContent] =
+    authenticatedActionWithSessionData.async { implicit request: RequestWithSessionData[AnyContent] =>
+        withAnswers[YesNo] { (_, answer) =>
+          val emptyForm  = northernIrelandForm
+          val filledForm = answer.fold(emptyForm)(emptyForm.fill)
+          Ok(northernIrelandAnswerPage(filledForm, Some("multiple"), postAction)).asFuture
+        }
+      }
+  val submit: Action[AnyContent] =
+    authenticatedActionWithSessionData
+      .async { implicit request =>
+        withAnswersAndRoutes[YesNo] { (fillingOutClaim, previousAnswer, routes) =>
+          import routes._
+
+          northernIrelandForm
+            .bindFromRequest()
+            .fold(
+              formWithErrors => BadRequest(northernIrelandAnswerPage(formWithErrors, Some("multiple"), postAction)).asFuture,
+              currentAnswer => {
+                val updatedJourney = from(fillingOutClaim)(
+                  _.copy(
+                    whetherNorthernIrelandAnswer = Some(currentAnswer),
+                    basisOfClaimAnswer = (previousAnswer, currentAnswer) match {
+                      case (Some(No), Yes)                                                              => None
+                      case (Some(Yes), No) if fillingOutClaim.draftClaim.hasNorthernIrelandBasisOfClaim => None
+                      case _                                                                            =>
+                        fillingOutClaim.draftClaim.basisOfClaimAnswer
+                    }
+                  )
+                )
+
+                EitherT(updateSession(sessionStore, request)(_.copy(journeyStatus = Some(updatedJourney))))
+                  .leftMap(_ => Error("could not update session"))
+                  .fold(
+                    logAndDisplayError("could not capture select number of claims"),
+                    _ =>
+                      Redirect(
+                        CheckAnswers.when(updatedJourney.draftClaim.isComplete)(alternatively =
+                          OverpaymentsRoutes.SelectBasisForClaimController.selectBasisForClaim(journeyBindable)
+                        )
+                      )
+                  )
+              }
+            )
+        }
+      }
+}
