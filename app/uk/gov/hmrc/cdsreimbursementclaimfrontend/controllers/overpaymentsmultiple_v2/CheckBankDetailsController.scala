bba6f12ce66ab586c517c1ce86b364a0054debaa
Artur Opala <250927+arturopala@users.noreply.github.com>
2023-02-28 08:31:03 +0000

    [CDSR-2126] add CheckBankDetailsController to overpayments multiple v2

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/overpaymentsmultiple_v2/CheckBankDetailsController.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/overpaymentsmultiple_v2/CheckBankDetailsController.scala
@@ -16,17 +16,44 @@
 
 package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.overpaymentsmultiple_v2
 
+import com.github.arturopala.validator.Validator.Validate
 import com.google.inject.Inject
 import com.google.inject.Singleton
+import play.api.mvc._
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.config.ViewConfig
 import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.JourneyControllerComponents
-import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.mixins.WorkInProgressMixin
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.OverpaymentsMultipleJourney
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.OverpaymentsMultipleJourney.Checks._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.BankAccountDetails
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.{upscan => _}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.common.check_bank_account_details
 
 import scala.concurrent.ExecutionContext
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.mixins.CheckBankDetailsMixin
 
 @Singleton
 class CheckBankDetailsController @Inject() (
-  val jcc: JourneyControllerComponents
-)(implicit val viewConfig: ViewConfig, val ec: ExecutionContext)
+  val jcc: JourneyControllerComponents,
+  val checkBankAccountDetailsPage: check_bank_account_details
+)(implicit val ec: ExecutionContext, val viewConfig: ViewConfig)
     extends OverpaymentsMultipleJourneyBaseController
-    with WorkInProgressMixin {}
+    with CheckBankDetailsMixin {
+
+  // Allow actions only if the MRN and ACC14 declaration are in place, and the EORI has been verified.
+  final override val actionPrecondition: Option[Validate[OverpaymentsMultipleJourney]] =
+    Some(hasMRNAndDisplayDeclaration & declarantOrImporterEoriMatchesUserOrHasBeenVerified)
+
+  final override def continueRoute(journey: Journey): Call =
+    if (journey.userHasSeenCYAPage) checkYourAnswers
+    else routes.ChooseFileTypeController.show
+
+  final override val chooseBankAccountTypeRoute: Call =
+    routes.ChooseBankAccountTypeController.show
+
+  final override val changeBankAccountDetailsRoute: Call =
+    chooseBankAccountTypeRoute
+
+  final override def modifyJourney(journey: Journey, bankAccountDetails: BankAccountDetails): Either[String, Journey] =
+    journey.submitBankAccountDetails(bankAccountDetails)
+
+}
