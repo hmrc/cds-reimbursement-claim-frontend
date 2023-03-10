3da0e33b6da029458b9c569718ec170ce954dfe8
Jamal Osman <jamal.osman93@gmail.com>
2023-03-10 15:02:12 +0000

    CDSR-2133: upload mrn list controller

---
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/overpaymentsscheduled_v2/UploadMrnListControllerSpec.scala
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.overpaymentsscheduled_v2
+
+import org.scalatest.BeforeAndAfterEach
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.libs.json.Json
+import play.api.mvc.{Call, Result}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.cache.SessionCache
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.connectors.UploadDocumentsConnector
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.{AuthSupport, PropertyBasedControllerSpec, SessionSupport}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.OverpaymentsScheduledJourney
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.OverpaymentsScheduledJourneyGenerators._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.{Feature, SessionData, _}
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.services.FeatureSwitchService
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.Future
+
+class UploadMrnListControllerSpec
+    extends PropertyBasedControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with BeforeAndAfterEach {
+
+  val mockUploadDocumentsConnector: UploadDocumentsConnector = mock[UploadDocumentsConnector]
+
+  val expectedUploadDocumentsLocation: String = "http://foo:123/bar/upload-mrn-list"
+
+  def mockInitializeCall(existingFile: Option[UploadedFile] = None) =
+    (mockUploadDocumentsConnector
+      .initialize(_: UploadDocumentsConnector.Request)(_: HeaderCarrier))
+      .expects(where { case (request, _) =>
+        request.existingFiles.map(_.upscanReference) == existingFile.map(_.upscanReference).toList
+      })
+      .returning(Future.successful(Some(expectedUploadDocumentsLocation)))
+
+  override val overrideBindings: List[GuiceableModule] =
+    List[GuiceableModule](
+      bind[AuthConnector].toInstance(mockAuthConnector),
+      bind[SessionCache].toInstance(mockSessionCache),
+      bind[UploadDocumentsConnector].toInstance(mockUploadDocumentsConnector)
+    )
+
+  val controller: UploadMrnListController = instanceOf[UploadMrnListController]
+
+  private lazy val featureSwitch = instanceOf[FeatureSwitchService]
+
+  override def beforeEach(): Unit =
+    featureSwitch.enable(Feature.Overpayments_v2)
+
+  "UploadMrnListController" when {
+
+    "Show page" must {
+
+      def performAction(): Future[Result] = controller.show()(FakeRequest())
+
+      "not find the page if overpayments v2 feature is disabled" in {
+        featureSwitch.disable(Feature.Overpayments_v2)
+        status(performAction()) shouldBe NOT_FOUND
+      }
+
+      "redirect to 'Upload Mrn List' when no file uploaded yet" in {
+        val journey = OverpaymentsScheduledJourney.empty(exampleEori)
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey))
+          mockInitializeCall()
+        }
+
+        checkIsRedirect(
+          performAction(),
+          Call("GET", s"$expectedUploadDocumentsLocation")
+        )
+      }
+
+      "redirect to 'Upload Mrn List' when some file uploaded already" in {
+        val journey = OverpaymentsScheduledJourney.empty(exampleEori)
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey.receiveScheduledDocument(journey.answers.nonce, uploadDocument).getOrFail))
+          mockInitializeCall(Some(uploadDocument))
+        }
+
+        checkIsRedirect(
+          performAction(),
+          Call("GET", s"$expectedUploadDocumentsLocation")
+        )
+      }
+
+      "redirect to 'Upload Mrn List' if journey has complete answers" in {
+        forAll(completeJourneyGen) { journey =>
+          inSequence {
+            mockAuthWithNoRetrievals()
+            mockGetSession(SessionData(journey))
+            mockInitializeCall(journey.answers.scheduledDocument)
+          }
+
+          checkIsRedirect(
+            performAction(),
+            Call("GET", s"$expectedUploadDocumentsLocation")
+          )
+        }
+      }
+
+    }
+
+    "'Upload Mrn List' submitted the callback" must {
+
+      def performAction(callback: UploadMrnListCallback): Future[Result] =
+        controller.submit()(FakeRequest().withJsonBody(Json.toJson(callback)))
+
+      val callbackPayload: UploadMrnListCallback =
+        UploadMrnListCallback(
+          nonce = Nonce.random,
+          uploadedFiles = Seq(uploadDocument)
+        )
+
+      "return 204 if callback accepted" in {
+        val journey = OverpaymentsScheduledJourney.empty(exampleEori)
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey))
+          mockStoreSession(
+            SessionData(
+              journey
+                .receiveScheduledDocument(journey.answers.nonce, uploadDocument)
+                .getOrFail
+            )
+          )(Right(()))
+        }
+        val result  = performAction(callbackPayload.copy(nonce = journey.answers.nonce))
+        status(result) shouldBe 204
+      }
+
+      "return 400 if callback rejected because of invalid nonce" in {
+        val journey = OverpaymentsScheduledJourney.empty(exampleEori)
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(
+            SessionData(journey)
+          )
+        }
+        val result  = performAction(callbackPayload.copy(nonce = Nonce.random))
+        status(result) shouldBe 400
+      }
+
+      "return 400 if callback rejected because of invalid request" in {
+        val journey = OverpaymentsScheduledJourney.empty(exampleEori)
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(
+            SessionData(journey)
+          )
+        }
+        val result  = controller.submit()(FakeRequest().withJsonBody(Json.parse("""{"foo":"bar"}""")))
+        status(result) shouldBe 400
+      }
+
+    }
+
+  }
+
+}
