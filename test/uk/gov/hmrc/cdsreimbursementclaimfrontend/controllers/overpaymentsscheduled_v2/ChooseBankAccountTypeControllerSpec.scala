02bc47210071583d90bdc68b479561c809e1d4d6
Haseem Isaac <95173192+haseemtasheel@users.noreply.github.com>
2023-03-18 08:04:03 +0000

    CDSR-2145: ChooseBankAccountTypeController test and functionality tested (#1350)

---
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/overpaymentsscheduled_v2/ChooseBankAccountTypeControllerSpec.scala
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.overpaymentsscheduled_v2
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.i18n.Lang
+import play.api.i18n.Messages
+import play.api.i18n.MessagesApi
+import play.api.i18n.MessagesImpl
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.cache.SessionCache
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.AuthSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.PropertyBasedControllerSpec
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.controllers.SessionSupport
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.journeys.OverpaymentsScheduledJourneyGenerators._
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.BankAccountType
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.Feature
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.SessionData
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.generators.BankAccountGen.arbitraryBankAccountType
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.services.FeatureSwitchService
+
+import scala.concurrent.Future
+
+class ChooseBankAccountTypeControllerSpec
+    extends PropertyBasedControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with BeforeAndAfterEach
+    with ScalaCheckPropertyChecks {
+
+  override val overrideBindings: List[GuiceableModule] =
+    List[GuiceableModule](
+      bind[AuthConnector].toInstance(mockAuthConnector),
+      bind[SessionCache].toInstance(mockSessionCache)
+    )
+
+  val session: SessionData = SessionData(journeyWithMrnAndDeclaration)
+
+  val controller: ChooseBankAccountTypeController = instanceOf[ChooseBankAccountTypeController]
+
+  val formKey: String = "select-bank-account-type"
+
+  implicit val messagesApi: MessagesApi = controller.messagesApi
+  implicit val messages: Messages       = MessagesImpl(Lang("en"), messagesApi)
+
+  private lazy val featureSwitch = instanceOf[FeatureSwitchService]
+
+  override def beforeEach(): Unit = featureSwitch enable Feature.Overpayments_v2
+
+  "Choose Bank Account Type Controller" should {
+
+    "show page" when {
+      def performAction(): Future[Result] =
+        controller.show()(FakeRequest())
+
+      "do not find the page if Overpayments v2 feature is disabled" in {
+        featureSwitch.disable(Feature.Overpayments_v2)
+
+        status(performAction()) shouldBe NOT_FOUND
+      }
+
+      "display the page on a new journey" in {
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$formKey.title"),
+          doc => {
+            selectedRadioValue(doc) shouldBe None
+            formAction(doc)         shouldBe routes.ChooseBankAccountTypeController.submit.url
+          }
+        )
+      }
+
+      "display the page on a pre-existing journey" in forAll { bankAccountType: BankAccountType =>
+        val journey = session.overpaymentsScheduledJourney.get.submitBankAccountType(bankAccountType).getOrFail
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(SessionData(journey))
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$formKey.title"),
+          doc => {
+            selectedRadioValue(doc) shouldBe Some(s"$bankAccountType")
+            formAction(doc)         shouldBe routes.ChooseBankAccountTypeController.submit.url
+          }
+        )
+      }
+    }
+
+    "submit page" when {
+
+      def performAction(data: (String, String)*): Future[Result] =
+        controller.submit()(FakeRequest().withFormUrlEncodedBody(data: _*))
+
+      "do not find the page if Overpayments V2 feature is disabled" in {
+        featureSwitch.disable(Feature.Overpayments_v2)
+
+        status(performAction()) shouldBe NOT_FOUND
+      }
+
+      "reject the submit if no entry chosen" in {
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey(s"$formKey.title"),
+          doc => getErrorSummary(doc) shouldBe messageFromMessageKey(s"$formKey.error.required"),
+          expectedStatus = BAD_REQUEST
+        )
+      }
+
+      "successfully supply an answer" in forAll { bankAccountType: BankAccountType =>
+        val updatedJourney = session.overpaymentsScheduledJourney.get.submitBankAccountType(bankAccountType).getOrFail
+        val updatedSession = session.copy(overpaymentsScheduledJourney = Some(updatedJourney))
+
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+          mockStoreSession(updatedSession)(Right(()))
+        }
+
+        checkIsRedirect(
+          performAction(formKey -> s"$bankAccountType"),
+          routes.EnterBankAccountDetailsController.show
+        )
+      }
+    }
+  }
+}
