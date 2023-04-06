03a9f9a940f114cd97094b025ccdb7fc157f0d1b
Artur Opala <250927+arturopala@users.noreply.github.com>
2023-04-06 15:15:22 +0200

    [CDSR-2183] add connector to /cds-reimbursement-claim/eori/xi (#1362)

---
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/connectors/XiEoriConnector.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.connectors
+
+import akka.actor.ActorSystem
+import cats.syntax.eq._
+import com.google.inject.ImplementedBy
+import com.google.inject.Inject
+import play.api.Configuration
+import play.api.libs.json.Format
+import play.api.libs.json.Json
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.ids.Eori
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.utils.Logging
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.HttpClient
+import uk.gov.hmrc.http.HttpResponse
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
+import javax.inject.Singleton
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+@ImplementedBy(classOf[DefaultXiEoriConnector])
+trait XiEoriConnector {
+
+  def getXiEori(implicit hc: HeaderCarrier): Future[Option[XiEoriConnector.Response]]
+}
+
+object XiEoriConnector {
+
+  final case class Response(eoriGB: Eori, eoriXI: Eori)
+  final case class Exception(msg: String) extends scala.RuntimeException(msg)
+
+  implicit val format: Format[Response] = Json.format[Response]
+}
+
+@Singleton
+class DefaultXiEoriConnector @Inject() (
+  http: HttpClient,
+  servicesConfig: ServicesConfig,
+  configuration: Configuration,
+  val actorSystem: ActorSystem
+)(implicit
+  ec: ExecutionContext
+) extends XiEoriConnector
+    with Retries
+    with Logging {
+
+  lazy val baseUrl: String = servicesConfig.baseUrl("cds-reimbursement-claim")
+
+  lazy val contextPath: String =
+    servicesConfig.getConfString("cds-reimbursement-claim.context-path", "cds-reimbursement-claim")
+
+  lazy val url = s"$baseUrl$contextPath/eori/xi"
+
+  lazy val retryIntervals: Seq[FiniteDuration] =
+    Retries.getConfIntervals("cds-reimbursement-claim", configuration)
+
+  override def getXiEori(implicit hc: HeaderCarrier): Future[Option[XiEoriConnector.Response]] =
+    retry(retryIntervals: _*)(shouldRetry, retryReason)(
+      http.GET[HttpResponse](url)
+    ).flatMap {
+      case response if response.status === 200 =>
+        Future(Some(response.json.as[XiEoriConnector.Response]))
+          .transform(identity, e => new XiEoriConnector.Exception(e.getMessage))
+
+      case response if response.status === 204 =>
+        Future.successful(None)
+
+      case response =>
+        Future.failed(
+          new XiEoriConnector.Exception(s"Request to POST $url failed because of ${response.status} ${response.body}")
+        )
+    }
+
+}
