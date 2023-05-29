829b3754510b0e809afe053342037636a483ca33
Tony Merryfield <tony.merryfield@oddbunch.co.uk>
2023-05-29 22:51:54 +0100

    CDSR-2285 - Update frontend library version (#1397)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-28" % "7.14.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc"         % "7.3.0-play-28",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc"         % "7.7.0-play-28",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-28"         % hmrcMongoPlayVersion,
     "org.typelevel"         %% "cats-core"                  % "2.8.0",
     "com.github.kxbmap"     %% "configs"                    % "0.5.0",
