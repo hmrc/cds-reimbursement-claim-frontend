884180879e7f3d9f8804ac86ecceedf5192c38de
Artur Opala <250927+arturopala@users.noreply.github.com>
2023-06-30 10:59:57 +0100

    [CDSR-2307] add order to the basis of claim list

--- test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoods/ChooseHowManyMrnsControllerSpec.scala
+++ test/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoods/ChooseHowManyMrnsControllerSpec.scala
@@ -57,7 +57,6 @@ import uk.gov.hmrc.cdsreimbursementclaimfrontend.views.html.rejectedgoods.choose
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.jdk.CollectionConverters._
-import scala.annotation.nowarn
 
 class ChooseHowManyMrnsControllerSpec
     extends ControllerSpec
