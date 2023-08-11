230da5aa6019ce5d5a60aaed87ace8768a760abc
MadStu <737342+MadStu@users.noreply.github.com>
2023-08-11 15:22:59 +0100

    Remove bootstrap-test-play-28 from AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,10 +7,9 @@ object AppDependencies {
   val monocleVersion       = "2.1.0"
   val jsoupVersion         = "1.16.1"
   val hmrcMongoPlayVersion = "1.3.0"
-  val bootStrapPlayVersion = "7.19.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-frontend-play-28" % bootStrapPlayVersion,
+    "uk.gov.hmrc"           %% "bootstrap-frontend-play-28" % "7.19.0",
     "uk.gov.hmrc"           %% "play-frontend-hmrc"         % "7.14.0-play-28",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-28"         % hmrcMongoPlayVersion,
     "org.typelevel"         %% "cats-core"                  % "2.9.0",
@@ -20,7 +19,6 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootStrapPlayVersion % Test,
     "org.scalatest"          %% "scalatest"               % "3.2.16"             % Test,
     "org.jsoup"               % "jsoup"                   % jsoupVersion         % Test,
     "com.typesafe.play"      %% "play-test"               % current              % Test,
