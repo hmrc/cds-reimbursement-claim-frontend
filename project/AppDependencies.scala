9e97233e10e2e70c80c1f929ec347ff3aab28a96
Artur Opala <250927+arturopala@users.noreply.github.com>
2024-02-19 12:11:48 +0000

    upgrade to tudor crown

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-28" % "8.1.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc"         % "7.29.0-play-28",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-28" % "8.5.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-28"         % hmrcMongoPlayVersion,
     "org.typelevel"         %% "cats-core"                  % "2.10.0",
     "com.github.kxbmap"     %% "configs"                    % "0.6.1",
