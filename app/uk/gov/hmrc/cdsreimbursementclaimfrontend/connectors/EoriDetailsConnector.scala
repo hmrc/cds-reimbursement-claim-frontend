a68c2f8513d366650ce5e50126ee8176b1949900
Artur Opala <250927+arturopala@users.noreply.github.com>
2024-10-16 21:33:34 +0100

    [CDSR-3338] add backend connector calling /eori endpoint

---
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/connectors/EoriDetailsConnector.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cdsreimbursementclaimfrontend.connectors
+
+import cats.syntax.eq._
+import com.google.inject.ImplementedBy
+import com.google.inject.Inject
+import org.apache.pekko.actor.ActorSystem
+import play.api.Configuration
+import play.api.libs.json.Format
+import play.api.libs.json.Json
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.models.ids.Eori
+import uk.gov.hmrc.cdsreimbursementclaimfrontend.utils.Logging
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.HttpClient
+import uk.gov.hmrc.http.HttpResponse
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
+import javax.inject.Singleton
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+@ImplementedBy(classOf[DefaultEoriDetailsConnector])
+trait EoriDetailsConnector {
+
+  def getEoriDetails(implicit hc: HeaderCarrier): Future[Option[EoriDetailsConnector.Response]]
+}
+
+object EoriDetailsConnector {
+
+  final case class Response(eoriGB: Eori, eoriXI: Option[Eori], fullName: String, eoriEndDate: Option[String])
+  final case class Exception(msg: String) extends scala.RuntimeException(msg)
+
+  implicit val format: Format[Response] = Json.format[Response]
+}
+
+@Singleton
+class DefaultEoriDetailsConnector @Inject() (
+  http: HttpClient,
+  servicesConfig: ServicesConfig,
+  configuration: Configuration,
+  val actorSystem: ActorSystem
+)(implicit
+  ec: ExecutionContext
+) extends EoriDetailsConnector
+    with Retries
+    with Logging {
+
+  lazy val baseUrl: String = servicesConfig.baseUrl("cds-reimbursement-claim")
+
+  lazy val contextPath: String =
+    servicesConfig.getConfString("cds-reimbursement-claim.context-path", "cds-reimbursement-claim")
+
+  lazy val url = s"$baseUrl$contextPath/eori"
+
+  lazy val retryIntervals: Seq[FiniteDuration] =
+    Retries.getConfIntervals("cds-reimbursement-claim", configuration)
+
+  override def getEoriDetails(implicit hc: HeaderCarrier): Future[Option[EoriDetailsConnector.Response]] =
+    retry(retryIntervals: _*)(shouldRetry, retryReason)(
+      http.GET[HttpResponse](url)
+    ).flatMap {
+      case response if response.status === 200 =>
+        Future(response.json.as[EoriDetailsConnector.Response])
+          .transform(r => Some(r), e => new EoriDetailsConnector.Exception(e.getMessage))
+
+      case response if response.status === 204 =>
+        Future.successful(None)
+
+      case response =>
+        Future.failed(
+          new EoriDetailsConnector.Exception(
+            s"Request to POST $url failed because of ${response.status} ${response.body}"
+          )
+        )
+    }
+
+}
