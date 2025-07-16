0d244b528a4b1f83aae8dd066188e8462192dd75
Artur Opala <250927+arturopala@users.noreply.github.com>
2025-07-16 18:52:07 +0100

    upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,8 @@ object AppDependencies {
   val hmrcMongoPlayVersion = "2.6.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % "9.14.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "12.6.0",
+    "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % "9.16.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "12.7.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "org.typelevel"         %% "cats-core"                  % "2.13.0",
     "com.github.arturopala" %% "validator"                  % "0.23.0"
@@ -22,7 +22,7 @@ object AppDependencies {
     "org.playframework"      %% "play-test"               % current              % Test,
     "org.scalamock"          %% "scalamock"               % "7.3.2"              % Test,
     "org.scalatestplus"      %% "scalacheck-1-18"         % "3.2.19.0"           % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.1"              % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2"              % Test,
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.8"             % Test,
     "org.scalameta"          %% "munit-diff"              % "1.0.4"              % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % hmrcMongoPlayVersion % Test
