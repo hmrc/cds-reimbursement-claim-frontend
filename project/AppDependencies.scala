467c4facfa06beb74e76edfb7599eb1aba945be3
Artur Opala <250927+arturopala@users.noreply.github.com>
2025-07-31 09:24:29 +0100

    [CDSR-4122] Implement new gov.uk branding across the service

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % "9.16.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "12.7.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "12.8.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "org.typelevel"         %% "cats-core"                  % "2.13.0",
     "com.github.arturopala" %% "validator"                  % "0.23.0"
