0b3058ea204023718c0bcc9e8b10c9c28532e944
Unmesh Malvankar <unmesh.malvankar1@digital.hmrc.gov.uk>
2025-12-30 20:18:58 +0000

    DCWL-4077 Removes 'security-reasons.nidac.mdp' feature flag

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt.*
 object AppDependencies {
 
   val jsoupVersion         = "1.21.2"
-  val hmrcMongoPlayVersion = "2.10.0"
-  val bootstrapVersion     = "10.4.0"
+  val hmrcMongoPlayVersion = "2.11.0"
+  val bootstrapVersion     = "10.5.0"
   val playSuffix           = "-play-30"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.20.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.25.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "org.typelevel"     %% "cats-core"                  % "2.13.0"
   )
