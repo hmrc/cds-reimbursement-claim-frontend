3cd3c8ad0ce324ed894e081b659d50d3a3af1144
rameshuk <ramesh.sukka@gmail.com>
2026-03-11 14:26:14 +0000

    DCWL-4585: updated allowed file extensions

--- app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsscheduled/UploadMrnListController.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaimfrontend/controllers/rejectedgoodsscheduled/UploadMrnListController.scala
@@ -121,7 +121,7 @@ class UploadMrnListController @Inject() (
       maximumFileSizeBytes = fileUploadConfig.readMaxFileSize("schedule-of-mrn"),
       allowedContentTypes =
         "application/pdf,image/jpeg,image/png,text/csv,text/plain,application/vnd.ms-outlook,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.oasis.opendocument.text,application/vnd.oasis.opendocument.spreadsheet",
-      allowedFileExtensions = ".pdf,.png,.jpg,.jpeg,.csv,.txt,.msg,.pst,.ost,.eml,.doc,.docx,.xls,.xlsx,.ods,.odt",
+      allowedFileExtensions = ".pdf,.png,.jpg,.jpeg,.csv,.txt,.msg,.doc,.docx,.xls,.xlsx,.ods,.odt",
       prePopulateYesOrNoForm = None,
       cargo = Some(UploadDocumentType.ScheduleOfMRNs),
       newFileDescription =
