bbca4d10d6f2cc3c1b58f6751bbf761ea2147e64
sergi-hmrc <97970044+sergi-hmrc@users.noreply.github.com>
2026-08-28 13:20:43 +0100

    DCWL-4864: add cds-reimbursement-claim-ui-tests to repository.yaml (#2192)
    * DCWL-4864: add cds-reimbursement-claim-ui-tests to repository.yaml
    
    * DCWL-4864: update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt.*
 
 object AppDependencies {
 
-  val jsoupVersion         = "1.21.2"
+  val jsoupVersion         = "1.23.2"
   val hmrcMongoPlayVersion = "2.13.0"
   val bootstrapVersion     = "10.7.0"
   val playSuffix           = "-play-30"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.9.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.11.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "org.typelevel"     %% "cats-core"                  % "2.13.0"
   )
@@ -23,7 +23,7 @@ object AppDependencies {
     "org.scalatestplus"      %% "scalacheck-1-18"             % "3.2.19.0"           % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"          % "7.0.2"              % Test,
     "com.vladsch.flexmark"    % "flexmark-all"                % "0.64.8"             % Test,
-    "org.scalameta"          %% "munit-diff"                  % "1.3.1"              % Test,
+    "org.scalameta"          %% "munit-diff"                  % "1.3.5"              % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"     % hmrcMongoPlayVersion % Test,
     "uk.gov.hmrc"            %% s"bootstrap-test$playSuffix"  % bootstrapVersion     % Test
   )
